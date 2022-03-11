e66943c3f204447009c6c7d5a62d5c3f80df7645
vanshianec <ivaniovov@abv.bg>
2022-03-11 23:52:25 +0200

    [NA-5] Add custom colors and app theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MealComponent } from './core/meal/meal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {MealComponent} from './core/diary/meal/meal.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from "@angular/material/button";
 import {MatTableModule} from '@angular/material/table';
-import { FoodTableComponent } from './core/meal/content/food-table/food-table.component';
+import {FoodTableComponent} from './core/diary/meal/content/food-table/food-table.component';
 import {MatIconModule} from "@angular/material/icon";
-import { DiaryComponent } from './core/diary/diary.component';
+import {DiaryComponent} from './core/diary/diary.component';
 import {MatDividerModule} from "@angular/material/divider";
+import {HeaderComponent} from './core/diary/header/header.component';
+import {PieChartModule} from "@swimlane/ngx-charts";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @NgModule({
   declarations: [
@@ -18,6 +21,7 @@ import {MatDividerModule} from "@angular/material/divider";
     MealComponent,
     FoodTableComponent,
     DiaryComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,9 +30,12 @@ import {MatDividerModule} from "@angular/material/divider";
     MatButtonModule,
     MatTableModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    PieChartModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/core/diary/meal/content/food-table/food-table.component.ts
+++ src/app/core/diary/meal/content/food-table/food-table.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {Food} from "../../../../shared/models/food";
+import {Food} from "../../../../../shared/models/food";
 import {ColumnData} from "./column-data";
 
 /**
